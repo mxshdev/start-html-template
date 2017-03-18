99724ba94774a21fb744e963e6e064d43fb1f16d
Shmasya <me@shmasya.ru>
2017-03-18 14:11:36 +0300

    Improve: add html templates, refactor code, and add new file structure. Add new libs.

--- gulpfile.js
+++ gulpfile.js
@@ -12,59 +12,104 @@ var gulp         = require('gulp'),
 		insert       = require('gulp-insert'),
 		fileInclude  = require('gulp-file-include'),
 		argv         = require('yargs').argv,
-		sass         = require('gulp-sass');
+		sass         = require('gulp-sass'),
+		zip          = require('gulp-zip'),
+		fs           = require('fs');
+
+/* ––––– Build settings ––––– */
+
+var srcPath  = './src';
+var destPath = './dest';
+
+if (argv.srcPath == 'this') {
+	srcPath  = '..';
+	destPath = '..';
+}
 
 /* ======================================== >>>>> */
 /* = Browser Sync Init = */
 /* ======================================== >>>>> */
 
-gulp.task('browser-sync', ['styles-main', 'scripts-main', 'scripts-libs'], function() {
+gulp.task('browser-sync', ['build-styles', 'build-scripts', 'build-scripts-libs', 'build-templates', 'copy-assets'], function() {
 	browserSync.init({
 		server: {
-			baseDir: "./app"
+			baseDir: destPath
 		},
 		notify: false
 	});
 });
 
 /* ======================================== >>>>> */
-/* = General styles = */
+/* = Build styles = */
 /* ======================================== >>>>> */
 
-gulp.task('styles-main', function() {
-	return gulp.src('./app/assets/css/app.scss')
+gulp.task('build-styles', function() {
+	return gulp.src(srcPath + '/assets/css/app.scss')
 						 .pipe(sass().on('error', onError))
 						 .pipe(autoprefixer({ browsers: ['last 15 versions'], cascade: false }))
-						 .pipe(gulp.dest('app/assets/css'))
+						 .pipe(gulp.dest(destPath + '/assets/css'))
 						 .pipe(csso())
 						 .pipe(rename({ suffix: '.min', prefix: '' }))
-						 .pipe(gulp.dest('app/assets/css'))
+						 .pipe(gulp.dest(destPath + '/assets/css'))
 						 .pipe(browserSync.stream());
 });
 
 /* ======================================== >>>>> */
-/* = General scripts = */
+/* = Build scripts = */
 /* ======================================== >>>>> */
 
-gulp.task('scripts-main', function() {
-	return gulp.src('./app/assets/js/app.js')
+gulp.task('build-scripts', function() {
+	return gulp.src(srcPath + '/assets/js/app.js')
+						 .pipe(gulp.dest(destPath + '/assets/js'))
 						 .pipe(rename({ suffix: '.min', prefix: '' }))
 						 .pipe(uglify())
 						 .on('error', onError)
-						 .pipe(gulp.dest('./app/assets/js/'));
+						 .pipe(gulp.dest(destPath + '/assets/js'));
 });
 
 /* ======================================== >>>>> */
-/* = Library scripts = */
+/* = Copy assets = */
 /* ======================================== >>>>> */
 
-gulp.task('scripts-libs', function() {
-	return gulp.src(['./app/assets/js/libs.js'])
+gulp.task('copy-assets', function() {
+	// Copy fonts
+	gulp.src(srcPath + '/assets/font/**')
+			.pipe(gulp.dest(destPath + '/assets/font'));
+	
+	// Copy and optimize images
+	gulp.src(srcPath + '/assets/img/**')
+			.pipe(gulp.dest(destPath + '/assets/img'));
+	
+	// Copy php files
+	gulp.src(srcPath + '/assets/php/**')
+			.pipe(gulp.dest(destPath + '/assets/php'));
+	
+	// Copy libs
+	gulp.src(srcPath + '/assets/libs/**')
+			.pipe(gulp.dest(destPath + '/assets/libs'));
+});
+
+/* ======================================== >>>>> */
+/* = Build templates = */
+/* ======================================== >>>>> */
+
+gulp.task('build-templates', function() {
+	return gulp.src(srcPath + '/templates/*.html')
+						 .pipe(fileInclude())
+						 .pipe(gulp.dest(destPath));
+});
+
+/* ======================================== >>>>> */
+/* = Build libs scripts = */
+/* ======================================== >>>>> */
+
+gulp.task('build-scripts-libs', function() {
+	return gulp.src([srcPath + '/assets/js/libs.js'])
 						 .pipe(rename({ suffix: '.min', prefix: '' }))
 						 .pipe(fileInclude())
 						 .pipe(uglify())
 						 .on('error', onError)
-						 .pipe(gulp.dest('./app/assets/js/'));
+						 .pipe(gulp.dest(destPath + '/assets/js'));
 });
 
 /* ======================================== >>>>> */
@@ -72,10 +117,21 @@ gulp.task('scripts-libs', function() {
 /* ======================================== >>>>> */
 
 gulp.task('watch', function() {
-	gulp.watch('./app/assets/css/*.scss', ['styles-main']);
-	gulp.watch('./app/assets/js/app.js', ['scripts-main']);
-	gulp.watch('./app/assets/js/*.js').on('change', browserSync.reload);
-	gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+	gulp.watch(srcPath + '/assets/css/*.scss', ['build-styles']);
+	gulp.watch(srcPath + '/assets/js/app.js', ['build-scripts']);
+	gulp.watch(srcPath + '/**/*.html', ['build-templates']);
+	gulp.watch(srcPath + '/assets/js/*.js').on('change', browserSync.reload);
+	gulp.watch(destPath + '**/*.html').on('change', browserSync.reload);
+});
+
+/* ======================================== >>>>> */
+/* = Create build archive = */
+/* ======================================== >>>>> */
+
+gulp.task('zip-build', function() {
+	return gulp.src(['./**', '!' + destPath + '/**', '!./node_modules/**', '!./node_modules', '!' + destPath])
+						 .pipe(zip('src.zip'))
+						 .pipe(gulp.dest('./'));
 });
 
 /* ======================================== >>>>> */
@@ -92,4 +148,4 @@ function onError(err) {
 /* ======================================== >>>>> */
 
 gulp.task('default', ['browser-sync', 'watch']);
-gulp.task('build', ['styles-main', 'scripts-main', 'scripts-libs']);
+gulp.task('build', ['build-styles', 'build-scripts', 'build-scripts-libs', 'build-templates', 'copy-assets', 'zip-build']);
